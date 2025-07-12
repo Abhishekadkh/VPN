7719793490c55e6621a79ede7fdef507c7258dc4
KhagendraN <nkhagendra1@gmail.com>
2025-07-12 14:54:13 +0545

    logger system tested and is completely working

--- server/main.cpp
+++ server/main.cpp
@@ -1,1 +1,20 @@
 // Accepts TLS connections, reads and injects packets.
+
+// <=====================TESTING LOGGER========================================================>
+
+#include "Logger.h"
+//#include <iostream>
+using namespace std;
+
+int main(){
+    Logger::Info("<========================TESTING LOGGER=======================================>");
+    Logger::Inti(LogLevel::DEBUG);
+    Logger::Info(" Server has been started!");
+    Logger::Status(ConnectionState::CONNECTING);
+    Logger::Warning(" Failed to create socked fd!!");
+    Logger::Status((ConnectionState::CONNECTED));
+    Logger::Debug(" Failed to bind socket fd!");
+    Logger::Error(" Couldn't start server!");
+    Logger::Status(ConnectionState::DISCONNECTED);
+    return 0;
+}
