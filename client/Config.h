764ee406d195d18c34b85b393f01fd320fcc5279
KhagendraN <nkhagendra1@gmail.com>
2025-07-13 20:30:55 +0545

    dev: Configs updated for clients

--- client/Config.h
+++ client/Config.h
@@ -1,1 +1,31 @@
 // Stores client configs (server IP, port, certs, TUN name).
+
+#pragma once 
+
+#include <string>
+
+class ClientConfig {
+public:
+    ClientConfig(const std::string& configPath = "");
+
+    // TUN device
+    std::string tunName;
+
+    // Server info
+    std::string serverIP;
+    int serverPort;
+
+    // TLS (Client side)
+    std::string caCertPath;
+    std::string clientCertPath;
+    std::string clientKeyPath;
+
+    // AES
+    std::string aesKey;
+    std::string aesIV;
+
+private:
+    void loadDefaults();
+    void loadFromFile(const std::string& path); 
+};
+
