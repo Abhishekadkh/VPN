eedfb1e524d24e407acf2fddadc70ceb938b8421
KhagendraN <nkhagendra1@gmail.com>
2025-07-13 20:32:37 +0545

    dev: updated configs for server

--- server/Config.h
+++ server/Config.h
@@ -1,1 +1,29 @@
 // Server port, certs, routing settings.
+
+#pragma once
+#include <string>
+
+class ServerConfig {
+public:
+    ServerConfig(const std::string& configPath = "");
+
+    // TUN device
+    std::string tunName;
+
+    // Listener info
+    int listenPort;
+
+    // TLS (Server side)
+    std::string caCertPath;
+    std::string serverCertPath;
+    std::string serverKeyPath;
+
+    // AES
+    std::string aesKey;
+    std::string aesIV;
+
+private:
+    void loadDefaults();
+    void loadFromFile(const std::string& path); 
+};
+
